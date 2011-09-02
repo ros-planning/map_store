b3fb8bb7863b0e88d11c5f1da46c5e113f36e2d3
pratkanis <pratkanis@willowgarage.com>
2011-09-02 10:39:29 -0700

    Add roslaunch file to install maps

---
+++ scripts/add_map.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+PKG = 'map_store'
+import roslib; roslib.load_manifest(PKG)
+import rospy
+import sys
+import map_store.srv
+
+save_as = None
+try:
+    save_as = sys.argv[1]
+except:
+    print "You must specify a name for the map on the server"
+    sys.exit(1)
+
+print "Waiting for /dynamic_map..."
+rospy.wait_for_service('/dynamic_map')
+print "Waiting for /name_latest_map..."
+rospy.wait_for_service('/name_latest_map')
+print "Waiting for /list_last_maps..."
+rospy.wait_for_service('/list_last_maps')
+print "Waiting for /delete_map..."
+rospy.wait_for_service('/delete_map')
+
+print "Checking for duplicates..."
+list_last_maps = rospy.ServiceProxy('/list_last_maps', map_store.srv.ListLastMaps)
+delete_map = rospy.ServiceProxy('/delete_map', map_store.srv.DeleteMap)
+maps = []
+try:
+    maps = list_last_maps().map_list
+except:
+    print "Getting maps from the map_manager has failed"
+    sys.exit(2)
+
+for i in maps:
+    if (i.name == save_as):
+        print "Deleting map", i.map_id
+        delete_map(i.map_id)
+
+print "Starting service..."
+save_map = rospy.ServiceProxy('/name_latest_map', map_store.srv.NameLatestMap)
+print "Saving map as", save_as
+save_map(save_as)
+print "Done"
+
